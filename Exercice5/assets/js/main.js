87f37a61e337ef81fc0a5af2b2c0d0998b3a0d65
Mehdi-Belgacem <72203119+Mehdi-Belgacem@users.noreply.github.com>
2020-11-19 11:11:27 +0100

    correction de la parti 6

--- Exercice5/assets/js/main.js
+++ Exercice5/assets/js/main.js
@@ -5,8 +5,8 @@ let result = firstNumber * secondNumber;
 
 while (result <= 250) {
     secondNumber = result;
-    result = firstNumber * secondNumber;
     let li = document.createElement('li');
     li.textContent = result;
     ul.appendChild(li);
+    result = firstNumber * secondNumber;
 }
--- Exercice6/assets/js/main.js
+++ Exercice6/assets/js/main.js
@@ -1,15 +1,12 @@
-let firstNumber = 3
-let secondNumber = 2
+let firstNumber = 8 ;
+let secondNumber = 2 ;
 let ul = document.getElementById('numberList');
 let result = firstNumber / secondNumber;
 
 while ( result > 1 ) {
     firstNumber = result;
-    result = firstNumber / secondNumber;
     let li = document.createElement('li');
     li.textContent = firstNumber;
+    result = firstNumber / secondNumber;
     ul.appendChild(li);
-    break 
-} 
-
-
+}
